87fc7fbb64bf1a032f56d0c2d8af98a216098617
Szymon Bacanski <s.bacanski@gmail.com>
2022-09-22 00:11:39 +0200

    basic validation for auth
    Added two functions: validateRegister and validateLogin

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User.js');
+const registerValidation = require('../validation/authValidation.js');
+const loginValidation = require('../validation/authValidation.js');
 const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
 	console.log('SERVER GOT ORDER: "register"');
 
-	// TODO: add validation to request - by JOI
+	const { error } = registerValidation(req.body);
+	if (error) {
+		console.log('REQUEST DATA ERROR');
+		console.log(error.details[0].message);
+		return res.json({ status: true, message: error.details[0].message });
+	}
+
 	let req_email = req.body.email;
 	let req_password = req.body.password;
 
@@ -22,8 +30,8 @@ router.post('/register', async (req, res) => {
 
 		await newUser.save();
 		console.log('New User: ', newUser);
-
 		console.log('User created');
+
 		res.json({ status: true, message: 'User created' });
 	} catch (err) {
 		res.json({ status: false, message: err });
@@ -33,7 +41,13 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
 	console.log('SERVER GOT ORDER: "login"');
 
-	// TODO: add validation to request - by JOI
+	const { error } = loginValidation(req.body);
+	if (error) {
+		console.log('REQUEST DATA ERROR');
+		console.log(error.details[0].message);
+		return res.json({ status: true, message: error.details[0].message });
+	}
+
 	let req_email = req.body.email;
 	let req_password = req.body.password;
 
@@ -49,8 +63,6 @@ router.post('/login', async (req, res) => {
 		}
 
 		const token = jwt.sign(userFromRequest._id.toString(), process.env.TOKEN_SECRET);
-
-		console.log(token);
 
 		console.log('User log in');
 		res.header('Authorization', `Bearer ${token}`).json({
