d6ba52490abd052aea856dc397f00471192902e8
Szymon Bacanski <s.bacanski@gmail.com>
2023-01-27 00:18:19 +0100

    THE big refactor
    REFACTOR. New start with this project

--- utils/isUserExist.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/userModel.js');
-const isUserExist = async id => {
-	if (!(await User.findById(id))) {
-		return false;
-	}
-	return true;
-};
-
-module.exports = isUserExist;
--- validation/loginValidation.js
+++
@@ -1,9 +0,0 @@
-// const validateLogin = request_body => {
-// 	const loginValidationSchema = Joi.object({
-// 		email: Joi.string().email().required(),
-// 		password: Joi.string().min(5).max(128).required(),
-// 	});
-
-// 	return loginValidationSchema.validate(request_body);
-// };
-// module.exports = validateLogin;
