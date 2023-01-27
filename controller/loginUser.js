cf9328fbf9f7f58639e74511362c220bf2568ef8
Szymon Bacanski <s.bacanski@gmail.com>
2023-01-27 23:12:30 +0100

    Login and Register functionalities
    Added two new endpoints: login and register.
    Connecting to database is now in separate function.

---
+++ controller/loginUser.js
@@ -0,0 +1,32 @@
+import jsonwebtoken from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import UserModel from '../models/userModel.js';
+import HttpError from '../utils/HttpError.js';
+import response from '../utils/response.js';
+
+export const loginUser = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		const loggingUser = await UserModel.findOne({ email: email });
+
+		if (!loggingUser) {
+			throw new HttpError("This user doesn't exist", 400);
+		}
+
+		if (!(await bcrypt.compare(password, loggingUser.password))) {
+			throw new HttpError('Wrong password', 400);
+		}
+
+		console.log(loggingUser._id.toString());
+
+		const token = jsonwebtoken.sign(
+			loggingUser._id.toString(),
+			process.env.PROJECT_SECRET
+		);
+
+		res.status(202).header('token', token).json(response(true, 'User logged'));
+	} catch (error) {
+		res.status(error.code).json(response(false, error.message));
+	}
+};
--- index.js
+++ index.js
@@ -3,17 +3,16 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
+import connectDatabase from './utils/connectDatabase.js';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 app.use('/auth', authRoutes);
 
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.CONNECTION_URL, () => console.log('Database Connected'));
+connectDatabase(() => console.log('Database Connected'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening at port ${PORT}`));
