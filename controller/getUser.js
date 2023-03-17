6d08cd1593a59c2a819ca6b20bbed2da5d5e187e
Szymon Bacanski <s.bacanski@gmail.com>
2023-03-17 15:45:26 +0100

    Prettier and eslint configured
    Prettier and eslint configured with changes enforced by them

--- controller/getUser.js
+++ controller/getUser.js
@@ -1,38 +1,34 @@
-import UserModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import UserModel from '../models/userModel.js';
 import response from '../utils/response.js';
 import HttpError from '../utils/HttpError.js';
 
-export const getUser = async (req, res) => {
-	try {
-		const { token } = req.headers;
+const getUser = async (req, res) => {
+  try {
+    const { token } = req.headers;
 
-		const { id } = jwt.verify(
-			token,
-			process.env.PROJECT_SECRET,
-			function (err, decoded) {
-				if (err) {
-					throw new HttpError(err.message, 401);
-				}
+    const { id } = jwt.verify(token, process.env.PROJECT_SECRET, (err, decoded) => {
+      if (err) {
+        throw new HttpError(err.message, 401);
+      }
 
-				return decoded;
-			}
-		);
+      return decoded;
+    });
 
-		console.log('id: ', id);
+    if (!id) {
+      throw new HttpError('Token invalid', 400);
+    }
 
-		if (!id) {
-			throw new HttpError('Token invalid', 400);
-		}
+    const user = await UserModel.findById(id);
 
-		const user = await UserModel.findById(id);
+    if (!user) {
+      throw new HttpError("This user doesn't exist", 400);
+    }
 
-		if (!user) {
-			throw new HttpError("This user doesn't exist", 400);
-		}
-
-		res.status(200).json(response(true, 'User info in content', user));
-	} catch (error) {
-		res.status(error.code).json(response(false, error.message));
-	}
+    res.status(200).json(response(true, 'User info in content', user));
+  } catch (error) {
+    res.status(error.code).json(response(false, error.message));
+  }
 };
+
+export default getUser;
--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-	email: {
-		type: String,
-		required: [true, 'Email is required'],
-		minLength: [6, 'Email has to be at least 6 characters long'],
-		maxLength: [30, "Email can't be longer than 30 character"],
-	},
-	password: {
-		type: String,
-		required: [true, 'Password is needed'],
-		minLength: [5, 'Password has to be at least 6 characters long'],
-		maxLength: [60, "Password can't be longer than 30 character"],
-	},
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    minLength: [6, 'Email has to be at least 6 characters long'],
+    maxLength: [30, "Email can't be longer than 30 character"],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is needed'],
+    minLength: [5, 'Password has to be at least 6 characters long'],
+    maxLength: [60, "Password can't be longer than 30 character"],
+  },
 });
 
 const UserModel = mongoose.model('User', userSchema);
