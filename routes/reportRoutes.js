80d5450b392ec27f165460f8ba030bb0ac9d6aed
Szymon Bacanski <s.bacanski@gmail.com>
2023-05-15 13:51:44 +0200

    routes and delete endpoint hotfix

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -7,8 +7,8 @@ import getReport from '../controller/getReport.js';
 const router = express.Router();
 
 router.post('/add', addReport);
-router.post('/delete/:id', deleteReport);
-router.post('/update', updateReport);
+router.delete('/delete/:id', deleteReport);
+router.put('/update', updateReport);
 router.get('/:id', getReport);
 
 export default router;
