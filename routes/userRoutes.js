624843a4a9cfeece857198d2630d9a3881720e44
Szymon Bacanski <s.bacanski@gmail.com>
2023-05-19 21:38:45 +0200

    Employee model
    New employee model and addEmployee endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ import getUser from '../controller/getUser.js';
 import getAllReports from '../controller/getAllReports.js';
 import emailVerification from '../controller/emailVerification.js';
 import sendEmailAgain from '../controller/sendEmailAgain.js';
+import addEmployee from '../controller/addEmployee.js';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get('/get-user', getUser);
 router.get('/get-all-reports', getAllReports);
 router.get('/verify/:id/:token', emailVerification);
 router.get('/send-email-again/:id/:token', sendEmailAgain);
+router.post('/add-employee', addEmployee);
 
 export default router;
