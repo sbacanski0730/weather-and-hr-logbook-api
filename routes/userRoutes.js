d8e76f10af2d9f132bf30d26433fb75a1afa7614
Szymon Bacanski <72625642+sbacanski0730@users.noreply.github.com>
2023-05-22 14:31:04 +0200

    #46-delete-employee-and-add-employee

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ import getAllReports from '../controller/getAllReports.js';
 import emailVerification from '../controller/emailVerification.js';
 import sendEmailAgain from '../controller/sendEmailAgain.js';
 import addEmployee from '../controller/addEmployee.js';
+import getEmployees from '../controller/getEmployees.js';
+import deleteEmployee from '../controller/deleteEmployee.js';
 
 const router = express.Router();
 
@@ -13,5 +15,7 @@ router.get('/get-all-reports', getAllReports);
 router.get('/verify/:id/:token', emailVerification);
 router.get('/send-email-again/:id/:token', sendEmailAgain);
 router.post('/add-employee', addEmployee);
+router.get('/employees', getEmployees);
+router.delete('/delete-employee/:id', deleteEmployee);
 
 export default router;
